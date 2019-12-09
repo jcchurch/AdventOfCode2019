214f7fcbe34d0784284cbc516b964767736bea2e
jcchurch@gmail.com <James Church>
2019-12-09 17:18:14 -0600

    Adds updated Sensor Boost application

--- Day09/SensorBoost.py
+++ Day09/SensorBoost.py
@@ -2,79 +2,88 @@ import sys
 import itertools
 
 class IntComputer:
-    def __init__(self, code):
+    def __init__(self, code, in_sequence=[]):
         self.code = { i: code[i] for i in range(len(code)) }
-        self.index = 0
+        self.instruction_pointer = 0
         self.relative_base = 0
+        self.in_sequence = in_sequence
+        self.in_index = 0
 
-    def __get(self, paramOffset):
-        mode = self.code[self.index] // (10 ** (1+paramOffset)) % 10
-        address = -1
-        if mode == 0:
-            address = self.code[self.index + paramOffset]
-        elif mode == 1:
-            address = self.index + paramOffset
-        elif mode == 2:
-            address = self.relative_base + self.code[self.index + paramOffset]
-        else:
-            raise Exception("invalid mode. mode={}".format(mode))
+    def __getMode(self, offset):
+        return self.code[self.instruction_pointer] // (10 ** (1+offset)) % 10
+
+    def __get(self, offset):
+        address = [
+                   self.code[self.instruction_pointer + offset], 
+                   self.instruction_pointer + offset,
+                   self.relative_base + self.code[self.instruction_pointer + offset]
+                  ][ self.__getMode(offset) ]
         assert address >= 0
         return self.code.get(address, 0)
 
-    def __write(self, position, value):
+    def __write(self, offset, value):
         assert type(value) == type(0)
+        position = self.instruction_pointer + offset
         assert position >= 0
-        assert self.code[position] >= 0
-        self.code[self.code[position]] = value
 
-    def run(self, in_sequence=[]):
-        in_index = 0
-        while self.code[self.index] != 99:
-            op = self.code[self.index] % 100
-            print("INDEX", self.index, "OP", op, "RB", self.relative_base)
+        address = [
+                   self.code[position],
+                   -1,
+                   self.relative_base + self.code[position]
+                  ][ self.__getMode(offset) ]
+        assert address >= 0
+        self.code[address] = value
+
+    def full_run(self):
+        outs = []
+        value = self.run()
+        while value != None:
+            outs.append(value)
+            value = cpu.run()
+        return outs
+
+    def run(self):
+        while self.code[self.instruction_pointer] != 99:
+            op = self.code[self.instruction_pointer] % 100
             if op == 1:
-                self.__write(self.index + 3, self.__get(1) + self.__get(2))
-                self.index += 4
+                self.__write(3, self.__get(1) + self.__get(2))
+                self.instruction_pointer += 4
             if op == 2:
-                self.__write(self.index + 3, self.__get(1) * self.__get(2))
-                self.index += 4
+                self.__write(3, self.__get(1) * self.__get(2))
+                self.instruction_pointer += 4
             if op == 3:
-                print("READING VALUE", in_sequence[in_index])
-                self.__write(self.index + 1, in_sequence[in_index])
-                in_index += 1
-                self.index += 2
+                self.__write(1, self.in_sequence[self.in_index])
+                self.in_index += 1
+                self.instruction_pointer += 2
             if op == 4:
                 value = self.__get(1)
-                self.index += 2
+                self.instruction_pointer += 2
                 return value
             if op == 5:
-                nextInstruction = self.index + 3
+                next_instruction_pointer = self.instruction_pointer + 3
                 if self.__get(1) != 0:
-                    nextInstruction = self.__get(2)
-                print("JUMP NOT ZERO to", nextInstruction, "(", self.__get(1), ")")
-                self.index = nextInstruction
+                    next_instruction_pointer = self.__get(2)
+                self.instruction_pointer = next_instruction_pointer
             if op == 6:
-                nextInstruction = self.index + 3
+                next_instruction_pointer = self.instruction_pointer + 3
                 if self.__get(1) == 0:
-                    nextInstruction = self.__get(2)
-                print("JUMP ZERO to", nextInstruction, "(", self.__get(1), ")")
-                self.index = nextInstruction
+                    next_instruction_pointer = self.__get(2)
+                self.instruction_pointer = next_instruction_pointer
             if op == 7:
                 valueToStore = 0
                 if self.__get(1) < self.__get(2):
                     valueToStore = 1
-                self.__write(self.index + 3, valueToStore)
-                self.index += 4
+                self.__write(3, valueToStore)
+                self.instruction_pointer += 4
             if op == 8:
-                print("IS", self.__get(1) ,"==", self.__get(2))
                 valueToStore = 0
                 if self.__get(1) == self.__get(2):
                     valueToStore = 1
-                self.__write(self.index + 3, valueToStore)
-                self.index += 4
+                self.__write(3, valueToStore)
+                self.instruction_pointer += 4
             if op == 9:
                 self.relative_base += self.__get(1)
-                self.index += 2
+                self.instruction_pointer += 2
 
 if __name__ == '__main__':
     code = []
@@ -82,8 +91,5 @@ if __name__ == '__main__':
         for line in file:
             code = [int(x) for x in line.split(",")]
 
-    cpu = IntComputer(code)
-    value = cpu.run([1])
-    while value != None:
-        print(value)
-        value = cpu.run()
+    cpu = IntComputer(code, [2])
+    print(cpu.full_run())
