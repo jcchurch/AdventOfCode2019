507044631966fd7c768c0ca6a6a4f27ae95c9309
jcchurch@gmail.com <James Church>
2019-12-11 08:23:24 -0600

    Adds documentation

--- Day11/IntComputer.py
+++ Day11/IntComputer.py
@@ -1,5 +1,17 @@
 class IntComputer:
     def __init__(self, code, in_sequence=[]):
+        """
+        This builds a new IntComputer object.
+        
+        `code` is a list of integers representing the code
+               that the IntComputer should execute.
+        `in_sequence` is a list of integer inputs that will
+               be sequenctially supplied to the computer
+               each time an input instruction is encountered.
+               Methods are available should you wish to supply
+               additional input after execution as started.
+               This parameter is optional.
+        """
         self.code = { i: code[i] for i in range(len(code)) }
         self.instruction_pointer = 0
         self.relative_base = 0
@@ -32,6 +44,10 @@ class IntComputer:
         self.code[address] = value
 
     def full_run(self):
+        """
+        Runs a computer until it stops.
+        Returns a list of the outputs.
+        """
         outs = []
         value = self.run()
         while value != None:
@@ -40,10 +56,22 @@ class IntComputer:
         return outs
 
     def add(self, value):
+        """
+        Adds a single value to the input stream.
+        """
         assert type(value) == type(0)
         self.in_sequence.append(value)
 
     def run(self):
+        """
+        Runs a computer until it stops or it outputs any data.
+        The internal state of the computer is kept so that this
+        function can be called again to resume execution.
+
+        This will return a single output upon an output instruction
+        or it will return None. None represents the conclusion
+        of the program's execution.
+        """
         while self.code[self.instruction_pointer] != 99:
             op = self.code[self.instruction_pointer] % 100
             if op == 1:
